ded5fecd7fac916f38bd57573b8ac74a735942ef
Peeter Piegaze <peeter.piegaze@sonarsource.com>
2020-08-18 17:17:25 +0200

    fix two code smells

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -4,8 +4,12 @@ package foo;
  * Foo class
  */
 public class Foo {
-
-    public static int div(int a, int b) throws Exception {
+    
+    private Foo() {
+        throw new IllegalStateException("Utility class");
+    }
+    
+    public static int div(int a, int b) throws UnsupportedOperationException {
     	if (b == 0) {
     		throw new UnsupportedOperationException("Can't divide by zero!");
     	}
